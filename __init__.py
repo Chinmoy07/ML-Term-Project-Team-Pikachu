1bc73cd7e0726ac8170096f1caf4d036026f609c
Jithin Sukumar <jithinsukumar23@gmail.com>
2016-10-12 09:56:51 +0530

    Program to extract tweets from Twitter API

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
 #program to extract tweets using Twitter API
-#recent 100 tweets that has 'finance' keyword in it
 
 import json
 import nltk
@@ -19,35 +18,61 @@ oauth = OAuthHandler(CONSUMER_TOKEN , CONSUMER_SECRET)
 oauth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 
 tweet_list = []        
+
+def get_proper_time( twitter_time, month_map ):
+    time_segments = twitter_time.split()
+    proper_time = time_segments[5]+'-'
+    proper_time += month_map[time_segments[1]]+'-'
+    proper_time += time_segments[2]+' '
+    proper_time += time_segments[3]    
+    return proper_time
+
+def compare_date( ):
+    return True        
         
 class listener(StreamListener):
-    count = 100
+    flag = True
+    count = 1000
+    start_date = "2016-10-08 05:15:36"
+    month_map = {
+        "Jan": "01",
+        "Feb": "02",
+        "Mar": "03",
+        "Apr": "04",
+        "May": "05",
+        "Jun": "06",
+        "Jul": "07",
+        "Aug": "08",
+        "Sep": "09",
+        "Oct": "10",
+        "Nov": "11",
+        "Dec": "12"
+    }
     def on_data(self, data):
         obj = json.loads(data)
-        if listener.count > 0 :
+        if ( listener.count > 0 ):
             try:
                 tweet_obj = {}
                 tweet_obj["text"] = obj["text"]
-                tweet_obj["date"] = obj["created_at"]
-                if( "finance" in tweet_obj["text"]):
-                    listener.count -= 1
-                    tweet_list.append( tweet_obj )
-                    print( str(listener.count) + "done")
-            except:
-                print(str(listener.count) + "not found")
+                tweet_obj["date"] = get_proper_time(obj["created_at"], listener.month_map ) 
+                tweet_list.append( tweet_obj )
+                print(listener.count)
+                listener.count = listener.count - 1
+            except Exception as err :
+                print(err)
         else:
             tweet_dict = {}
             tweet_dict["tweets"] = tweet_list
             f.write(json.dumps(tweet_dict))
             f.close()
             sys.exit()
+                            
         return(True)
 
     def on_error(self, status):
         print(status)
-
-
+        
 twitter_stream = Stream(oauth, listener())
-twitter_stream.filter(track=["finance"])
+twitter_stream.filter(track=["microsoft"], languages=["en"])
 f.close()
 
